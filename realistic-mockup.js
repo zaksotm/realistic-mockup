3e6f81dfe399c3eced1d061b3023c54530d2cc2a
zaksotm <61716255+zaksotm@users.noreply.github.com>
2020-04-15 15:47:34 -0500

    update

--- realistic-mockup.js
+++ realistic-mockup.js
@@ -13,25 +13,33 @@ function init() {
 
 	//create scene for 3D model
 	scene = new THREE.Scene();
+	scene.background = new THREE.Color( 0xffffff );
 	//camera = new THREE.PerspectiveCamera(1, window.innerWidth/window.innerHeight, 0.1, 1000);
-	camera = new THREE.PerspectiveCamera(1, 1, 0.1, 1000);
+	camera = new THREE.PerspectiveCamera(1, 1, 1, 1000);
 
+	/*
 	ambientLight = new THREE.AmbientLight(0x404040, 4.0);
 	scene.add(ambientLight);
+	*/
+
+	var hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+	hemiLight.position.set( 0, 1000, 0 );
+	scene.add( hemiLight );
+
 
 	//Create 3D model
 	//load gltf file
 	var gLoader = new THREE.GLTFLoader();
-	
+
 	console.log("ghjghj");
-	
-	gLoader.load('mockup_v1.12.gltf', function ( gltf ) {
+
+	gLoader.load('mockup_v2.06.gltf', function ( gltf ) {
 		scene.add( gltf.scene );
 		model = gltf.scene;
 		}
 	);
 
-	camera.position.set(0, 100, 0);
+	camera.position.set(0, 600, 0);
 	camera.lookAt(new THREE.Vector3(0,0,0));
 
 	renderer = new THREE.WebGLRenderer();
@@ -41,7 +49,7 @@ function init() {
 
 	document.body.appendChild(renderer.domElement);
 	//document.getElementById("canvasHolder").appendChild(renderer.domElement);
-	
+
 	function applyTexture(){
 		console.log("function applyTexture called");
 		var filesSelected = document.getElementById("inputFileToLoad").files;
@@ -54,53 +62,53 @@ function init() {
 					//Create texture from uploaded image
 					var tLoader = new THREE.TextureLoader();
 					bitmapTexture = tLoader.load(fileLoadedEvent.target.result);
-					
+
 					//Set bitmap properties
 					bitmapTexture.flipY = false;
 					bitmapTexture.generateMipmaps = false;
 					bitmapTexture.wrapS = bitmapTexture.wrapT = THREE.ClampToEdgeWrapping;
 					bitmapTexture.minFilter = THREE.LinearFilter;
 					bitmapTexture.encoding = THREE.sRGBEncoding;
-					
-					
+
+
 					//Create material from bitmap
 					var materialBitmap = new THREE.MeshStandardMaterial({
 						map: bitmapTexture,
-						opacity: 0.8,
-						transparent: true,
+						//opacity: 0.8,
+						//transparent: true,
 					});
-					
+
 					//Get and create material of Heel color
 					var heelColor = document.getElementById("heelColor");
 					var heelColorHex = heelColor.options[heelColor.selectedIndex].value;
 					var materialHeel = new THREE.MeshStandardMaterial({
 						color: parseInt(heelColorHex, 16),
-						opacity: 0.8,
-						transparent: true,
+						//opacity: 0.8,
+						//transparent: true,
 					});
-					
+
 					//Get and create material of Toe color
 					var toeColor = document.getElementById("toeColor");
 					var toeColorHex = toeColor.options[toeColor.selectedIndex].value;
 					var materialToe = new THREE.MeshStandardMaterial({
 						color: parseInt(toeColorHex, 16),
-						opacity: 0.8,
-						transparent: true,
+						//opacity: 0.8,
+						//transparent: true,
 					});
-					
+
 					//Get and create material of Cuff color
 					var cuffColor = document.getElementById("cuffColor");
 					var cuffColorHex = cuffColor.options[cuffColor.selectedIndex].value;
 					var materialCuff = new THREE.MeshStandardMaterial({
 						color: parseInt(cuffColorHex, 16),
-						opacity: 0.8,
-						transparent: true,
+						//opacity: 0.8,
+						//transparent: true,
 					});
-					
+
 					//Assign new Materials
 					model.children[2].children[0].material = materialBitmap;
-					model.children[2].children[1].material = materialHeel;
-					model.children[2].children[2].material = materialToe;
+					model.children[2].children[1].material = materialToe;
+					model.children[2].children[2].material = materialHeel;
 					model.children[2].children[3].material = materialCuff;
 				};
 				fileReader.readAsDataURL(fileToLoad);
@@ -111,6 +119,7 @@ function init() {
 
 
 function animate() {
+	//if (model) model.rotation.z += 0.001;
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
 }
